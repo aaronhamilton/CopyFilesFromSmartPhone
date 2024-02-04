25e21e505fd207ccf0262d20b9fb8265a74dc66e
Aaron Hamilton <aaronster@gmail.com>
2024-02-03 22:35:43 -0500

    improve error reporting and action output

---
+++ Copy.ps1
@@ -0,0 +1,10 @@
+﻿
+#net use W: /d
+#net use X: /d
+
+net use W: \\192.168.0.120\Volume_2 /user:aaron bartfast
+net use X: \\192.168.0.122\MyBookLive /user:ahamilton 1-am-Havelock!
+
+robocopy W:\Backups\MyBookLive\Public\public  X: /e /copy:dat /dcopy:T /r:30 /w:30 /LOG+:c:\robolog4.txt /ETA /BYTES /tee
+
+CaliperCowboy2009!
